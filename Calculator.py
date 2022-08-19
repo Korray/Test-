7620f910b73a5c6edfddcdd136bc13a2ecbc679b
Korray <111415906+Korray@users.noreply.github.com>
2022-08-19 14:40:35 +0300

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,15 @@
+
+print ('Канкулятор')
+Num1 = float (input ('Введите число '))
+Z =  input ('Введите знак ')
+Num2 = float (input ('Введите число '))
+X = ()
+if Z == "+":
+    X = Num1 + Num2
+elif Z == "-":
+    X = Num1 - Num2
+elif Z == "/":
+    X = Num1 / Num2
+elif Z == "*":
+    X = Num1 * Num2
+print ('ОТВЕТ ' + str(X))
